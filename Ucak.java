9b24801e8f6f50913f7ed1aa2c8de1cad0399147
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-08 02:36:19 +0300

    UcakUcreti

---
+++ Ucak.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Ucak {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        double toplam;
+        System.out.println("Yasınız:");
+        a = sc.nextInt();
+        System.out.println("Gidilecek yol(KM):");
+        b = sc.nextInt();
+        System.out.println("Tek yön ise 1, gidis gelis ise 2 yazınız:");
+        c = sc.nextInt();
+        if(a>0 && b>0 && c>0)
+        {
+            if(c==1)
+            {
+                if(a<12)
+                {
+                    toplam = b*1/20;
+                    System.out.println("Ucret:"+toplam);
+                }
+                if(a>=12 && a<=24)
+                {
+                    toplam = b*9/100;
+                    System.out.println("Ucret:"+toplam);
+                }
+                if(a>=65)
+                {
+                    toplam = b*7/100;
+                    System.out.println("Ucret:"+toplam);
+                }
+            }
+            else
+            {
+                if(a<12)
+                {
+                    toplam = b*4/100;
+                    System.out.println("Ucret:"+toplam);
+                }
+                if(a>=12 && a<=24)
+                {
+                    toplam = b*36/500;
+                    System.out.println("Ucret:"+toplam);
+                }
+                if(a>=65)
+                {
+                    toplam = b*28/500;
+                    System.out.println("Ucret:"+toplam);
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Hatali giris.");
+        }
+
+    }
+}
